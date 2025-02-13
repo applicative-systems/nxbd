cae644728eb34afb847e8c2d105896f62b437b28
Jacek Galowicz <jacek@galowicz.de>
2025-02-13 12:41:48 +0700

    Add treefmt stuff

--- flake.nix
+++ flake.nix
@@ -4,12 +4,26 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-    perSystem = { config, self', inputs', pkgs, system, ... }: {
+    perSystem = { config, self', inputs', pkgs, system, ... }: 
+      let
+        treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            deadnix.enable = true;
+            nixfmt.enable = true;
+            statix.enable = true;
+          };
+        };
+      in
+      {
       _module.args.pkgs = import inputs.nixpkgs {
         inherit system;
         overlays = [
@@ -29,6 +43,12 @@
           pkgs.clippy
           pkgs.cargo-edit
         ];
+      };
+
+      formatter = treefmtEval.config.build.wrapper;
+
+      checks = config.packages // {
+        formatting = treefmtEval.config.build.check inputs.self;
       };
     };
     flake = {
