df3f4312fc4085baef946a6b584fbd04a46e83f3
Jacek Galowicz <jacek@galowicz.de>
2025-02-17 11:20:08 +0000

    comment stuff

--- tests/switch-test.nix
+++ tests/switch-test.nix
@@ -36,8 +36,6 @@ in
           '';
         };
 
-        #system.includeBuildDependencies = true;
-
         virtualisation = {
           cores = 2;
           memorySize = 8000; # went OOM with lower values
@@ -72,12 +70,15 @@ in
     machine.succeed("cp ${./project-folder}/* .")
 
     machine.succeed("nix flake lock --override-input nixpkgs ${pkgs.path}")
-    print(machine.succeed("cat flake.lock"))
+
+    # let's build this with nix before we build it with nxbd again.
+    # in case it fails, we know if the config is broken or nxbd.
     machine.succeed("nix -L build .#nixosConfigurations.machine.config.system.build.toplevel")
 
+    # TODO: Actually make check return a nonzero return code.
+    # this way we can test if the ignore list works well.
     machine.succeed("nxbd check")
     machine.succeed("nxbd build")
     machine.succeed("nxbd switch-local")
-
   '';
 }
